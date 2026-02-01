a1aba78935316e7be9456275716600f0d2694524
Priyanshubhatt156 <priyanshubhatt2375@gmail.com>
2026-02-01 18:20:10 +0530

    Add files via upload

---
+++ cript.js
@@ -0,0 +1,7 @@
+// Smooth Scroll for Buttons
+document.querySelector('.btn-outline').addEventListener('click', () => {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+});
+
+// Simple Console Log to confirm site load
+console.log("Anuj Rawat's Portfolio Loaded successfully.");
